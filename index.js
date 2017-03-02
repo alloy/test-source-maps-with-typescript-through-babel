e952ffe91b03b3bd1b30663fa7e706ecc804e835
Eloy Durán <eloy.de.enige@gmail.com>
2017-03-03 00:47:09 +0100

    Finish example

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require("path")
 
 const SourceMapConsumer = require("source-map").SourceMapConsumer
 
-const babel = require("babel-core")
 const babelConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, "./.babelrc"), "utf-8"))
+const babelTransform = require("babel-core").transform
 const babelGenerate = require("babel-generator").default
 
 const ts = require("typescript")
@@ -14,44 +14,64 @@ const filename = path.resolve(__dirname, "./fixture.tsx")
 const source = fs.readFileSync(filename, "utf-8")
 
 const tsResult = ts.transpileModule(source, {compilerOptions: tsConfig.compilerOptions, fileName:filename})
-console.log(tsResult.outputText)
-// const tsSourceMap = JSON.parse(tsResult.sourceMapText)
-// console.log(tsSourceMap)
-
-const babelResult = babel.transform(tsResult.outputText, Object.assign({ filename: filename.replace(/tsx$/, 'js') }, babelConfig))
-console.log(babelResult)
-console.log(babelResult.ast.tokens.find(token => token.value === "MyComponent").loc)
-// console.log(babelResult.code)
-
-// The doc for babel-generator says we need to pass an object like the following, but that’s not true it wants a string.
-// const fileMap = {}
-// fileMap[filename] = source
-
-const generated = babelGenerate(babelResult.ast, {
+// This is what React Native does: https://github.com/facebook/react-native/blob/13b4c2d77b855a1e20b153700bdfa5106bfcc346/packager/transformer.js#L109-L116
+const transformResult = babelTransform(tsResult.outputText, Object.assign({ filename: filename.replace(/tsx$/, 'js') }, babelConfig))
+const generateResult = babelGenerate(transformResult.ast, {
   comments: false,
   compact: false,
   filename,
   sourceFileName: filename,
   sourceMaps: true,
-  // inputSourceMap: tsSourceMap,
-// }, tsResult.outputText)
 }, source)
 
-// console.log(generated.code)
-// console.log(JSON.stringify(generated.map))
+const transformSourceMap = new SourceMapConsumer(transformResult.map)
+const generateSourceMap = new SourceMapConsumer(generateResult.map)
 
-const sourceMap = new SourceMapConsumer(generated.map)
-const generatedPosition = sourceMap.generatedPositionFor({
-  source: filename,
-  line: 3,
-  column: 25,
-})
-// console.log(generatedPosition)
+function compare(line, column) {
+  const transformedPosition = transformSourceMap.generatedPositionFor({
+    source: path.basename(filename), // TODO inconsistent with full path used with `generateSourceMap`.
+    line,
+    column,
+  })
 
-function codeAt(code, line, startColumn, numberOfColumns) {
-  return code.split("\n")[line-1].substring(startColumn-1, startColumn+numberOfColumns-1)
+  const generatedPosition = generateSourceMap.generatedPositionFor({
+    source: filename,
+    line,
+    column,
+  })
+
+  function codeAt(code, line, startColumn, numberOfColumns = 20) {
+    return code.split("\n")[line-1].substring(startColumn-1, startColumn+numberOfColumns-1)
+  }
+
+  console.log(" Original: " + codeAt(source, line, column))
+  console.log("Transform: " + codeAt(transformResult.code, transformedPosition.line, transformedPosition.column))
+  console.log(" Generate: " + codeAt(generateResult.code, generatedPosition.line, generatedPosition.column))
+  console.log("")
 }
 
-console.log("")
-console.log("Expected: " + codeAt(source, 3, 25, 11))
-console.log("  Actual: " + codeAt(generated.code, generatedPosition.line, generatedPosition.column, 11))
+console.log("-----------------")
+console.log("Original")
+console.log("-----------------")
+console.log(source)
+
+console.log("-----------------")
+console.log("tsc output")
+console.log("-----------------")
+console.log(tsResult.outputText)
+
+console.log("----------------------")
+console.log("babel transform output")
+console.log("----------------------")
+console.log(transformResult.code)
+
+console.log("----------------------")
+console.log("babel generate output")
+console.log("----------------------")
+console.log(generateResult.code)
+
+console.log("-----------------")
+console.log("Assertions")
+console.log("-----------------")
+compare(1, 1)
+compare(3, 16)
